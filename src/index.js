7bab1e06ccf24eacc6a70be10a86be7385208eb1
Md. Rubayet Hassan <rubayet.evan@gmail.com>
2022-08-09 00:12:00 +0600

    project initialized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { lat: null };
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}),
+            error => console.error(error)
+        );
+    }
+
+    render(){
+        return <div>latitude: {this.state.lat}</div>
+    }
+}
+
+root.render(<App/>);
