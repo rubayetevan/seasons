855826caad705f88b1fdc5f6adaa4ef6dedd9377
Md. Rubayet Hassan <rubayet.evan@gmail.com>
2022-08-09 00:12:55 +0600

    code formatted

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { lat: null };
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude}),
-            error => console.error(error)
-        );
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => console.error(error)
+    );
+  }
 
-    render(){
-        return <div>latitude: {this.state.lat}</div>
-    }
+  render() {
+    return <div>latitude: {this.state.lat}</div>;
+  }
 }
 
-root.render(<App/>);
+root.render(<App />);
